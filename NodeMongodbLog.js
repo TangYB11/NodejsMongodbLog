f51c0c78bcf11ef43e66837c18d9a1ef3925e4dd
tyb <1845094938@qq.com>
2021-12-12 17:26:33 -0800

    mongodb success

--- NodeMongodbLog.js
+++ NodeMongodbLog.js
@@ -1,8 +1,23 @@
 const express = require("express")
 const app = express()
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://172.21.2.236:27017/190110910523');
+const schema={
+    name:String,
+    age:Number,
+    health:String,
+    hooby:String
+}
+const mydata = mongoose.model('catls', schema);
+// const kitty = new mydata({name :'testzzzz'});
+// kitty.save()
+
+
 app.use("/",express.static('public'))
 app.get("/input",(req,res)=>{
     res.send(req.query)
     console.log(req.query)
+    const kitty = new mydata({name: req.query.first,health:req.query.second});
+    kitty.save()
 })
 app.listen(50523)
